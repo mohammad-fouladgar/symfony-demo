ca928b9f44eb6097e13c583432d1ab92a073bfc9
mohammad-fouladgar <fouladgar.dev@gmail.com>
2020-02-02 12:36:05 +0330

    First commit

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+    * @Route("/lucky/number",name="aaa")
+    */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return $this->json(['data'=>['number'=>$number]]);
+    }
+}
